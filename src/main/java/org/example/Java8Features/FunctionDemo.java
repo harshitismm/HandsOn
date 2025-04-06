cff7429bace249e24d0876b942c67b2bdcb55ce9
harshit <harahitg@gmail.com>
2025-04-06 16:16:23 +0530

    Function Joining

---
+++ src/main/java/org/example/Java8Features/FunctionDemo.java
@@ -0,0 +1,15 @@
+package org.example.Java8Features;
+
+import java.util.function.Function;
+
+public class FunctionDemo {
+
+    public static void main (String[] args) {
+        Function<Integer, Integer> doubleIt = i -> 2 * i;
+        System.out.println("Double Function " + doubleIt.apply(2));
+        Function<Integer, Integer> cubeIt = i -> i * i * i;
+        System.out.println("Cube Function " + cubeIt.apply(2));
+        System.out.println("First Doubling using apply: " + doubleIt.andThen(cubeIt).apply(2));
+        System.out.println("First Cubing using compose: " + doubleIt.compose(cubeIt).apply(2));
+    }
+}
